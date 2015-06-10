91ea56364a353e392587fed332f9083d3ba9d45f
edwardbryant <edward.bryant@gmail.com>
2015-06-09 19:05:14 -0500

    v2 py class shell added

---
+++ reader-v2.py
@@ -0,0 +1,23 @@
+import webbrowser
+
+class Contract():
+
+    """ Class for representing an unstructured contract document """
+
+    def __init__(self, path, filename):
+        """ Inits a Contract object 
+        Args:
+        path = a string of the path to the contract
+        filename = a string of the contract's filename
+        # encoding = possible arg: encoding?
+        """
+        self.path = path
+        self.filename = filename
+
+    def show_document():
+        """ Opens contract in a web browser """
+        # webbrowser.open(self. )
+
+
+
+
